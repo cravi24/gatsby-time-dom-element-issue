9b63ca53b1efa8afe0ee58d9c0c579f0d73f58c7
Ravi Chaudhary <cravi24@gmail.com>
2022-05-16 10:27:04 +0200

    Initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import * as React from "react"
+
+import Layout from "../components/layout"
+
+const IndexPage = () => (
+  <Layout>
+    {/* <time dateTime="PT23S">23</time> */}
+    <div>df</div>
+  </Layout>
+)
+
+export default IndexPage
